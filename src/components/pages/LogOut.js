ffa14263b25d2970ecddeff44008a3c4110bb510
ElenaGospodinova <gospodinovaelena@yahoo.co.uk>
2023-05-17 00:28:57 +0100

    log-In-Out

--- src/components/pages/LogOut.js
+++ src/components/pages/LogOut.js
@@ -5,7 +5,7 @@ import { Link, useMatch, useResolvedPath } from "react-router-dom";
 const LogOut = () => {
     return  <> 
        
-      <h2 className="title">Leaving the stage, but the melody lingers on. Thank you for joining the symphony.</h2>
+      <h2 className="title">Leaving the stage, but the melody lingers on. Thank you for joining the symphony!</h2>
         <div class="card-container">
             <div class="front"><p className="out-text">Thank you for the musical moments!</p></div>
             <div class="back">
